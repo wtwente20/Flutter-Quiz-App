6266813facc8fea072d92be3001984426fa93b63
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-29 17:53:32 -0500

    created start_screen.dart, styled as desired

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 103, 58, 183),
+                Color.fromARGB(255, 75, 37, 139),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
