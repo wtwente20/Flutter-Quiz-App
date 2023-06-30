3101948f4ab00ff17f2163808b158aa6f2cdd325
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-30 14:38:37 -0500

    created models and data folders

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import 'package:quizapp/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What are the main building blocks of Flutter UIs?', [
+    'Widgets',
+    'Components',
+    'Blocks',
+    'Functions',
+  ])
+];
